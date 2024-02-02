3a09303b8bd84962987b1e215c75b445cb3be140
Sean Liang <sean.y.liang@gmail.com>
2024-02-02 10:44:16 -0500

    Fixed naming convention to consistently use snakecase. Use DECIMAL instead of INT for 'monthly_rent'. Fixed typos.

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ USE cisc_499;
 -- Drop tables if they exist to prevent errors
 DROP TABLE IF EXISTS rental_agreement;
 DROP TABLE IF EXISTS make_group;
-DROP TABLE IF EXISTS picture_files;
+DROP TABLE IF EXISTS pictures;
 DROP TABLE IF EXISTS own;
 DROP TABLE IF EXISTS manage;
 DROP TABLE IF EXISTS manager;
@@ -18,75 +18,75 @@ DROP TABLE IF EXISTS property;
 CREATE TABLE property (
     PID int NOT NULL AUTO_INCREMENT primary key,
     house_name varchar(255),
-    Address varchar(255),
+    address varchar(255),
     house_type varchar(255),
-    Monthly_rent int,
-    bedroom int,
-    bathroom int,
+    monthly_rent decimal,
+    bedrooms int,
+    bathrooms int,
     parking bool,
     laundry varchar(255),
     fenced_yard bool,
-    Detached varchar(255),
-    Floor int,
+    detached_or_semi varchar(255),
+    floors int,
     elevator bool,
     number_of_people int,
-    private_Kitchen bool,
-    Furniture varchar(255),
+    private_kitchen bool,
+    furniture varchar(255),
     date_listed date,
     stats varchar(255) 
 );
 
 CREATE TABLE Students (
     UID varchar(255) PRIMARY KEY,
-    FName varchar(255),
-    LName varchar(255),
-    PhoneNo varchar(255),
-    Address varchar(255),
-    Student_ID int,
-    Year_of_graduation date,
-    Program varchar(255)
+    first_name varchar(255),
+    last_name varchar(255),
+    phone_number varchar(255),
+    address varchar(255),
+    student_id int,
+    year_of_graduation date,
+    program varchar(255)
 );
 
 CREATE TABLE rental_group (
     GID varchar(4) PRIMARY KEY,
-    Preferred_type varchar(255),
-    Bedroom int,
-    Bathroom int,
-    Parking bool,
-    Laundry varchar(255),
-    Range_lower_bound int,
-    Range_upper_bound int,
+    preferred_type varchar(255),
+    bedrooms int,
+    bathrooms int,
+    parking bool,
+    laundry varchar(255),
+    range_lower_bound int,
+    range_upper_bound int,
     accessibility bool
 );
 
 CREATE TABLE credentials (
     UID varchar(255) PRIMARY KEY,
-    Password_ varchar(255),
-    Role_ varchar(255)
+    password_ varchar(255),
+    role_ varchar(255)
 );
 
 CREATE TABLE owner (
     OID varchar(255) PRIMARY KEY,
-    Fname varchar(255),
-    Lname varchar(255),
-    PhoneNo_ID varchar(255) UNIQUE,
-    Address varchar(255)
+    first_name varchar(255),
+    last_name varchar(255),
+    phone_number varchar(255) UNIQUE,
+    address varchar(255)
 );
 
 CREATE TABLE manager (
-    PhoneNumber varchar(255) PRIMARY KEY,
-    FName varchar(255),
-    LName varchar(255)
+    phone_number varchar(255) PRIMARY KEY,
+    first_name varchar(255),
+    last_name varchar(255)
 );
 
 CREATE TABLE manage (
     PID int,
-    PhoneNumber varchar(255),
-    StartYear date,
-    EndDate date,
-    PRIMARY KEY (PID, PhoneNumber),
+    phone_number varchar(255),
+    start_year date,
+    end_date date,
+    PRIMARY KEY (PID, phone_number),
     FOREIGN KEY (PID) REFERENCES property (PID),
-    FOREIGN KEY (PhoneNumber) REFERENCES manager (PhoneNumber)
+    FOREIGN KEY (phone_number) REFERENCES manager (phone_number)
 );
 
 CREATE TABLE own (
@@ -97,20 +97,20 @@ CREATE TABLE own (
     FOREIGN KEY (OID) REFERENCES owner (OID)
 );
 
-CREATE TABLE picture_files (
+CREATE TABLE pictures (
     PID int,
-    File_name varchar(255),
-    PRIMARY KEY (PID, File_name),
+    file_name varchar(255),
+    PRIMARY KEY (PID, file_name),
     FOREIGN KEY (PID) REFERENCES property (PID)
 );
 
 CREATE TABLE rental_agreement (
     PID int,
     GID varchar(4),
-    Sign_Data date,
-    End_Data date,
-    Start_Data date,
-    Monthly_rent varchar(255),
+    sign_date date,
+    end_date date,
+    start_date date,
+    monthly_rent decimal,
     PRIMARY KEY (PID, GID),
     FOREIGN KEY (PID) REFERENCES property (PID),
     FOREIGN KEY (GID) REFERENCES rental_group (GID)
@@ -120,11 +120,11 @@ CREATE TABLE make_group (
     UID varchar(255),
     GID varchar(4),
     PRIMARY KEY (UID, GID),
-    FOREIGN KEY (UID) REFERENCES Students (UID),
+    FOREIGN KEY (UID) REFERENCES students (UID),
     FOREIGN KEY (GID) REFERENCES rental_group (GID)
 );
 
-INSERT INTO property (house_name, Address, house_type, Monthly_rent, bedroom, bathroom, parking, laundry, fenced_yard, Detached, elevator, number_of_people, private_Kitchen, Furniture, date_listed)
+INSERT INTO property (house_name, address, house_type, monthly_rent, bedrooms, bathrooms, parking, laundry, fenced_yard, detached_or_semi, elevator, number_of_people, private_kitchen, furniture, date_listed)
 VALUES
 ('334 Kingscourt Ave', '334 Kingscourt Ave, # 1, Kingston, ON K7K 4R5', 'Apartment', 2349, 3, 1, true, 'shared', false, 'yes', false, NULL, true, 'bed, desk, chair', '2023-11-7'),
 ('631 Aylmer Cres', '631 Aylmer Cres, # 2, Kingston, ON K7M 6K3', 'Apartment', 1899, 2, 1, true, 'shared', true, 'yes', false, NULL, true, 'bed, desk, chair', '2023-11-13'),
@@ -134,7 +134,7 @@ VALUES
 
 UPDATE property
 SET stats = 'on market'
-WHERE house_name = '334 Kingscourt Ave' AND Address = '334 Kingscourt Ave, # 1, Kingston, ON K7K 4R5';
+WHERE house_name = '334 Kingscourt Ave' AND address = '334 Kingscourt Ave, # 1, Kingston, ON K7K 4R5';
 
 UPDATE property
 SET stats = 'pending'
